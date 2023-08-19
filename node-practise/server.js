a94835ad9b9b8967ed8eed57094d0a8f1fcd1c86
zjiaoyan <2653516511@qq.com>
2023-08-19 15:31:33 +0800

    feat: middleware practise

--- node-practise/server.js
+++ node-practise/server.js
@@ -8,7 +8,36 @@ const PORT = process.env.PORT || 3500;
 app.get("^/$|/index(.html)?", (req, res) => {
   // res.send("hello world");
   // res.sendFile('./views/index.html', {root: __dirname})
-  res.sendFile(path.join(__dirname, 'views', 'index.html'))
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.get(
+  "/hello(.html)?",
+  (req, res, next) => {
+    console.log("attampted to load hello.html");
+    next();
+  },
+  (req, res) => {
+    res.send("hello world");
+  }
+);
+
+const one = (req, res, next) => {
+  console.log("one");
+  next();
+};
+const two = (req, res, next) => {
+  console.log("two");
+  next();
+};
+const three = (req, res) => {
+  console.log("three");
+  res.send("finished");
+};
+app.get("/chain(.html)?", [one, two, three]);
+
+app.get("/*", (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

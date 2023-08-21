4a608c53419796649e180a2b3facf2de22d6ed73
zjiaoyan <2653516511@qq.com>
2023-08-21 16:59:26 +0800

    feat: add redirect file

--- node-practise/routes/root.js
+++ node-practise/routes/root.js
@@ -5,8 +5,11 @@ const path = require("path");
 router.get("^/$|/index(.html)?", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "views", "index.html"));
 });
-router.get("/old-page(.html)?", (req, res) => {
+router.get("/new-page(.html)?", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "views", "new-page.html"));
+});
+router.get("/old-page(.html)?", (req, res) => {
+  res.redirect(301, "/new-page.html");
 });
 
 // router.get(

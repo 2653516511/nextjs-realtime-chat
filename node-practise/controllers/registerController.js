83fc10b265486d0b09102b8d93ee6270c1cbe879
Joya <2653516511@qq.com>
2023-08-31 16:30:06 +0800

    feat: start to add mongoose

--- node-practise/controllers/registerController.js
+++ node-practise/controllers/registerController.js
@@ -1,13 +1,5 @@
-const fsPromises = require("fs").promises;
-const path = require("path");
+const User = require("../model/User");
 const bcrypt = require("bcrypt");
-
-const usersDB = {
-  users: require("../model/users.json"),
-  setUsers: function (data) {
-    this.users = data;
-  },
-};
 
 const handlerNewUser = async (req, res) => {
   const { user, pwd } = req.body;
@@ -18,7 +10,7 @@ const handlerNewUser = async (req, res) => {
   }
 
   // check for duplicate usernames in the db
-  const duplicate = usersDB.users.find((person) => person.username === user);
+  const duplicate = await User.findOne({ username: user }).exec();
   if (duplicate) {
     return res.sendStatus(409); // Conflict
   }
@@ -26,21 +18,17 @@ const handlerNewUser = async (req, res) => {
   try {
     // encrypt the password
     const hashedPwd = await bcrypt.hash(pwd, 10);
-    // store the new user
-    const newUser = {
+
+    // create and store the new user
+    const result = await User.create({
       username: user,
-      roles: { User: 2001 },
       password: hashedPwd,
-    };
-    usersDB.setUsers([...usersDB.users, newUser]);
-    await fsPromises.writeFile(
-      path.join(__dirname, "..", "model", "users.json"),
-      JSON.stringify(usersDB.users)
-    );
-    console.log(usersDB.users);
+    });
+
+    console.log("-----create", result);
 
     res.status(201).json({
-      success: `New user ${newUser} created!`,
+      success: `New user ${user} created!`,
     });
   } catch (err) {
     res.status(500).json({
